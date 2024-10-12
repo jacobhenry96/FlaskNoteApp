5b2d8c1358096715fa12389306adb19c2610eacc
jacobhenry96 <jacobkhenry2@gmail.com>
2024-10-11 21:30:41 -0500

    Add files via upload

---
+++ FlaskNoteApp/website/static/index.js
@@ -0,0 +1,9 @@
+console.log('Working')
+// function deleteNote(noteId) {
+//     fetch("/delete-note", {
+//       method: "POST",
+//       body: JSON.stringify({ noteId: noteId }),
+//   }).then((_res) => {
+//     window.location.href = "/";
+//   });
+//   }
